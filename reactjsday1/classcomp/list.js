8a83de99b1eec802875571b269412e370787dc9a
Adilakshmi <annapareddyadilakshmi2002@gmail.com>
2024-02-19 13:35:16 +0530

    Adding file

---
+++ reactjsday1/classcomp/list.js
@@ -0,0 +1,16 @@
+import React from 'react'
+class List extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>class component </h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default List;
---
+++ reactjsday1/functioncomp/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App(){
+    return(
+        <div>
+            <h1>functional component </h1>
+            <h3>This is h1 tag </h3>
+        </div>
+    )
+}
+export default App;
