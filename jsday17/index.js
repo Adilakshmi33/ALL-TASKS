b16a8e470a218f5ca97d9ceec14b892ad6ad1e79
Adilakshmi <annapareddyadilakshmi2002@gmail.com>
2024-03-06 11:02:58 +0530

    Adding file

---
+++ jsday17/index.js
@@ -0,0 +1,28 @@
+let inputEle=document.getElementById("location-input")
+let tempEle=document.getElementById("temp-value")
+let locEle=document.getElementById("location")
+let weatherdescEle=document.getElementById("weather-desc")
+let btnEle=document.getElementById("btn")
+const apiKey ="e9b3ae19b393b2349042ef9b95a373b7"
+btnEle.onclick=function(){
+    if(inputEle.value=="")
+    alert("Please Enter some location")
+    else{
+        loc=inputEle.value
+        url="https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}"
+        fetch(url).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            const{name}=data
+            const{feels_like}=data.main
+            const{description}=data.weather[0]
+            tempEle.innerText=math.floor(feels_like-273);
+            locEle.innerText=name;
+            weatherdescEle.innerText=description
+        })
+        .catch(()=>{
+            alert("Enter valid location")
+        })
+        inputEle.value=""
+    }
+}
